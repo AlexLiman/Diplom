ce4f7375fb613d4efbfc7d7bc039e9c4248fc9a9
AlexLiman <limanalex@mail.ru>
2016-05-07 20:46:04 +0300

    Initial Commit

---
+++ openCV-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openCV-test
+//
+//  Created by Alex on 07.05.16.
+//  Copyright © 2016 com.alexliman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
