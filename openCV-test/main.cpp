732e198ae0c453c38c31ac5836b84090c537cb07
AlexLiman <limanalex@mail.ru>
2016-05-11 08:06:48 +0300

    test

--- openCV-test/main.cpp
+++ openCV-test/main.cpp
@@ -13,6 +13,12 @@
 
 using namespace cv;
 using namespace std;
+RNG rng(12345);
+Mat src; Mat src_gray;
+int thresh = 100;
+int max_thresh = 255;
+
+void thresh_callback(int, void* );
 
 
 int main( int, char** argv )
@@ -36,11 +42,11 @@ int main( int, char** argv )
     //2. Convert to gray image and apply canny edge detection
     cvtColor( src_mat, gray_mat, COLOR_RGB2GRAY );
     Canny(gray_mat, canny_mat, 30, 128, 3, false);
-    
+//    vector< vector< cv::Point> > contours;
+//    findContours(contour_mat, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
     //3. Find & process the contours
     //3.1 find contours on the edge image.
-    vector< vector< cv::Point> > contours;
-    findContours(canny_mat, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    
 
     // Threshold.
     // Set values equal to or above 220 to 0.
@@ -50,7 +56,7 @@ int main( int, char** argv )
     
     // Floodfill from point (0, 0)
     Mat im_floodfill = im_th.clone();
-    floodFill(im_floodfill, cv::Point(0,0), Scalar(200));
+    floodFill(im_floodfill, cv::Point(0,0), Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) ));
     
     // Invert floodfilled image
     Mat im_floodfill_inv;
@@ -58,14 +64,27 @@ int main( int, char** argv )
     
     // Combine the two images to get the foreground.
     Mat im_out = (im_th | im_floodfill_inv);
+
+    src = im_out;
     
+    /// Convert image to gray and blur it
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+    blur( src_gray, src_gray, Size(3,3) );
+    
+    /// Create Window
+    const char* source_window = "Source";
+    namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+    imshow( source_window, src );
+    
+    createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
+    thresh_callback( 0, 0 );
     
     // Display images
-    imshow("Foreground", im_out);
-    imwrite("/Users/alex/Desktop/openCV-test/openCV-test/test.jpg", im_out);
+    //imshow("Foreground", im_out);
+    
     //3.2 draw contours & property value on the source image.
 //    double area, length;
-    //drawContours(contour_mat, contours, -1, cv::Scalar(123), 4);  //draw contours on the image
+//     drawContours(contour_mat, contours, -1, cv::Scalar(123), 4);  //draw contours on the image
 //    for(int i = 0; i < contours.size(); ++i)
 //    {
 //        Point2f center;
@@ -125,4 +144,44 @@ int main( int, char** argv )
     waitKey(0);
     
     return 0;
+}
+
+/** @function thresh_callback */
+void thresh_callback(int, void* )
+{
+    Mat threshold_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    
+    /// Detect edges using Threshold
+    threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
+    /// Find contours
+    findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    
+    /// Approximate contours to polygons + get bounding rects and circles
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    vector<Point2f>center( contours.size() );
+    vector<float>radius( contours.size() );
+    
+    for( int i = 0; i < contours.size(); i++ )
+    { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+        boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+        minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+    }
+    
+    
+    /// Draw polygonal contour + bonding rects + circles
+    Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+        rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+        circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+    }
+    
+    /// Show in a window
+    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    imshow( "Contours", drawing );
 }
